dd9609ebee1a20b49fbb33fa9e68aaabf5b52d25
Erkenbend <ad.game-adress@hotmail.fr>
2025-05-02 22:00:45 +0200

    add buttons left and right to add and remove choices

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,71 @@
-import {useState} from 'react'
+import {JSX, useState} from 'react'
 import './App.css'
 
 // TODO: add history of previous choices
-// TODO: make number of buttons configurable (+/- left and right)
 
 function App() {
     const [randomResult, updateRandomResult] = useState<number>(0)
-    const buttonMin = 2;
-    const buttonMax = 8;
+    const buttonMinDefault = 2;
+    const buttonMaxDefault = 8;
 
-    function generateButtons(min: number, max: number) {
+    const [bounds, setBounds] = useState<number[]>([buttonMinDefault, buttonMaxDefault]);
+    const [buttons, setButtons] = useState<JSX.Element[]>(generateButtons(buttonMinDefault, buttonMaxDefault));
+
+    function incrementRightSide() {
+        const buttonLabel = bounds[1] + 1;
+        const button = generateButton(buttonLabel);
+        setButtons(b => [...b, button]);
+        setBounds(b => [b[0], buttonLabel]);
+    }
+
+    function decrementRightSide() {
+        setButtons(b => b.slice(0, -1));
+        setBounds(b => [b[0], b[1] - 1]);
+    }
+
+    function incrementLeftSide() {
+        const buttonLabel = bounds[0] - 1;
+        const button = generateButton(buttonLabel);
+        setButtons(b => [button, ...b]);
+        setBounds(b => [buttonLabel, b[1]]);
+    }
+
+    function decrementLeftSide() {
+        setButtons(b => b.slice(1, b.length))
+        setBounds(b => [b[0] + 1, b[1]]);
+    }
+
+    function generateButtons(min: number, max: number): JSX.Element[] {
         const result = []
         for (let i = min; i <= max; i++) {
-            result.push(<button type="button" onClick={() => {
-                updateRandomResult(() => Math.ceil(Math.random() * i));
-            }}>{i}</button>)
+            result.push(generateButton(i))
         }
         return result
     }
 
+    function generateButton(label: number): JSX.Element {
+        return <button type="button" onClick={() => {
+            updateRandomResult(() => Math.ceil(Math.random() * label));
+        }}>{label}</button>
+    }
+
     return (
         <>
-            <h2>
-                Click on a button to randomly choose among that amount of options
-            </h2>
+            <div className="instructions">
+                <div className="size-changer">
+                    <button type="button" onClick={incrementLeftSide} disabled={bounds[0] <= 2}>+</button>
+                    <button type="button" onClick={decrementLeftSide} disabled={bounds[0] == bounds[1]}>-</button>
+                </div>
+                <div className="instructions-text">
+                    Click on a button to randomly choose among that amount of options
+                </div>
+                <div className="size-changer">
+                    <button type="button" onClick={decrementRightSide} disabled={bounds[0] == bounds[1]}>-</button>
+                    <button type="button" onClick={incrementRightSide} disabled={bounds[1] >= 20}>+</button>
+                </div>
+            </div>
             <div className="button-list">
-                {generateButtons(buttonMin, buttonMax)}
+                {buttons}
             </div>
             <h1 className="result-card">
                 <span className="result-text">Pick option </span><span
